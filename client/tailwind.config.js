b15de75fc7e4a6104d31522376120ff1bc55f8d7
M-Raza-07 <razaofficial180@gmail.com>
2025-09-18 17:53:20 +0530

    create the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,15 @@
-// import flowbite from "flowbite/plugin";
+import flowbite from "flowbite/plugin";
 
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//     "node_modules/flowbite-react/**/*.{js,jsx}",
-//     "node_modules/flowbite/**/*.{js,jsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [flowbite],
-// };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx}",
+    "node_modules/flowbite/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite],
+};
